86dda343fe36c70546058e251fb53e0f7819ce82
best_zba <916714692@qq.com>
2020-02-01 15:23:26 +0800

    微信小程序初体验

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,21 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  data:{
+    name:"张保安",
+    students:[
+      {id:110,name:"张三",age:18},
+      {id:111,name:"里斯",age:19}
+    ],
+    counter:0
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  handlebtnAdd(){
+    console.log('haha ')
+    this.setData({
+      counter:this.data.counter + 1
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  handlebtnSubtract(){
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      counter:this.data.counter - 1
     })
   }
 })
