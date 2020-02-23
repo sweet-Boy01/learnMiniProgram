1c2e6d5b742c2a80406cafa32c6aae87a71422be
best_zba <916714692@qq.com>
2020-02-23 12:31:00 +0800

    微信小程序弹窗的使用

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,61 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+const app = getApp();
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  handleShowToast(){
+    wx.showToast({
+      title: '哈哈哈',
+      duration:3000,
+      mask:true,
+      success:function(e){
+        console.log(e)
+      }
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+  handleShowModal(){
+    wx.showModal({
+      title: '我是标题',
+      content: '我是内容',
+      cancelText:"退出",
+      cancelColor:"red",
+      success:function(e){
+        console.log(e)
+        if(e.cancel){
+          console.log('点击了退出')
+        }else if(e.confirm){
+          console.log('点击了确定')
         }
-      })
-    }
+      }
+    })
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  handleShowLoading:function(){
+    wx.showLoading({
+      title: '正在加载',
+      success:function(e){
+        console.log(e)
+        setInterval(()=>{
+          wx.hideLoading();
+        },3000)
+      }
+    })
+  },
+  handleAction(){
+    wx.showActionSheet({
+      itemList: ['拍照','相册'],
+      success:function(e){
+        console.log(e)
+        switch(e.tapIndex){
+          case 0 :{console.log('点击了拍照')}
+          break;
+          case 1 :{console.log('点击了相册')}
+          break;
+        }
+      }
     })
   }
 })
