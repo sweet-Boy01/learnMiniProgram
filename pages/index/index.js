6fbd1a7c9b2b9db0b71b4213ea452eec0452f3a6
best_zba <916714692@qq.com>
2020-03-01 15:18:02 +0800

    登录流程

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,55 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
-Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+App({
+  globaData: {
+    token: ""
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  onLaunch: function() {
+    //获取token
+    const token = wx.getStorageSync('token')
+    if (token && token.length !== 0) { //判断有token
+      this.check_token(token)
+    } else { //判断没有token
+      this.login()
+    }
+
+  },
+  check_token(token){//验证token是否过期
+    console.log('执行验证操作')
+    wx.request({
+      url: 'http://123.207.32.32:3000/auth',
+      method:"post",
+      header:{token},
+      success:(res)=>{
+        if (!res.data.errCode) {//token没有过期
+          this.globalData.token = token
+        }else{//token过期
+          this.login()
+        }
+      }
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+  login() {
+    console.log('执行登录操作')
+    //获取code
+    wx.login({
+      success: (res) => {
+        console.log(res)
+        const code = res.code //code只有5分钟有效期
+        //将code发送给服务器，返回token
+        wx.request({
+          url: 'http://123.207.32.32:3000/login',
+          method: "post",
+          data: {
+            code
+          },
+          success: (res) => { //获取到token
+            const token = res.data.token;
+            //将token保存在本地
+            wx.setStorageSync('token', token)
+            //将token保存在全局
+            this.globalData.token = token
+          }
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
   }
 })
